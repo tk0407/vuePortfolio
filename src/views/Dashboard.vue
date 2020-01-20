a196dd121837e1c07e833bc8943a9c95e491de58
takamasa asai <tkasai0407@gmail.com>
2020-01-20 21:23:04 +0800

    finished lesson4 Layouts

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,51 +1,105 @@
 <template>
-  <div>
+  <v-container>
     <h1>Dashboard</h1>
-    <v-data-table
-      :headers="headers"
-      :items="employees"
-      :items-per-page="5"
-      class="elevation-1"
-      @click:row="selectRow"
-      :multi-sort="true"
-    ></v-data-table>
-    <v-snackbar v-model="snackbar">
+
+    <v-row>
+      <v-col v-for="sale in sales" :key="`${sale.title}`" cols="12" md="4">
+        <SalesGraph :sale="sale" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        v-for="statistic in statistics"
+        :key="`${statistic.title}`"
+        cols="12"
+        md="6"
+        lg="3"
+      >
+        <StatisticCard :statistic="statistic" />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row>
+
+    <!-- <v-row id="below-the-fold" v-intersect="showMoreContent">
+      <v-col cols="12" md="8">
+        <EmployeesTable :employees="employees" @select-employee="setEmployee" />
+      </v-col>
+      <v-col cols="12" md="4">
+        <EventTimeline :timeline="timeline" />
+      </v-col>
+    </v-row> -->
+
+    <v-row v-if="loadNewContent" id="more-content">
+      <v-col>
+        <v-skeleton-loader
+          ref="skeleton"
+          type="table"
+          class="mx-auto"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+
+    <v-snackbar v-model="snackbar" :left="$vuetify.breakpoint.lgAndUp">
       You have selected {{ selectedEmployee.name }},
       {{ selectedEmployee.title }}
       <v-btn color="pink" text @click="snackbar = false">
         Close
       </v-btn>
     </v-snackbar>
-  </div>
+  </v-container>
 </template>
 
 <script>
-import employeesData from "../data/employees.json";
+import EmployeesTable from '../components/EmployeesTable'
+import EventTimeline from '../components/EventTimeline'
+import SalesGraph from '../components/SalesGraph'
+import StatisticCard from '../components/StatisticCard'
+
+import employeesData from '../data/employees.json'
+import timelineData from '../data/timeline.json'
+import salesData from '../data/sales.json'
+import statisticsData from '../data/statistics.json'
 
 export default {
-  name: "DashboardPage",
+  name: 'DashboardPage',
+  components: {
+    EmployeesTable,
+    EventTimeline,
+    SalesGraph,
+    StatisticCard
+  },
   data() {
     return {
+      employees: employeesData,
+      loadNewContent: false,
+      sales: salesData,
       selectedEmployee: {
-        name: "",
-        title: ""
+        name: '',
+        title: ''
       },
-      snackbar: true,
-      headers: [
-        { text: "Employee ID", value: "id" },
-        { text: "Name", value: "name" },
-        { text: "Position Title", value: "title" },
-        { text: "Salary", value: "salary" }
-      ],
-      employees: employeesData
-    };
+      snackbar: false,
+      statistics: statisticsData,
+      timeline: timelineData
+    }
   },
   methods: {
-    selectRow(event) {
-      this.snackbar = true;
-      this.selectedEmployee.name = event.name;
-      this.selectedEmployee.title = event.title;
+    setEmployee(event) {
+      this.snackbar = true
+      this.selectedEmployee.name = event.name
+      this.selectedEmployee.title = event.title
+    },
+    showMoreContent(entries) {
+      this.loadNewContent = entries[0].isIntersecting
     }
   }
-};
+}
 </script>
